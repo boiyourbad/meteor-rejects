6db1a00d1f710a5d2976f616675e1e112db661de
Aaaaa <88433052+boiyourbad@users.noreply.github.com>
2022-12-07 08:16:40 +0700

    Delete src/main directory

--- src/main/java/anticope/rejects/events/PlayerRespawnEvent.java
+++
@@ -1,9 +0,0 @@
-package anticope.rejects.events;
-
-public class PlayerRespawnEvent {
-    private static final PlayerRespawnEvent INSTANCE = new PlayerRespawnEvent();
-
-    public static PlayerRespawnEvent get() {
-        return INSTANCE;
-    }
-}
